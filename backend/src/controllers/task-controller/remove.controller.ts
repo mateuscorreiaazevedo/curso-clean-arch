2331b142fd3829c762adf3ddf4cedb82c53b4ac5
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-04-21 01:49:54 -0300

    chore: refact controllers

--- backend/src/controllers/task-controller/remove.controller.ts
+++ backend/src/controllers/task-controller/remove.controller.ts
@@ -1,14 +1,18 @@
-import { Task } from "@/models/task-model";
+import { MongooseTaskRepository } from "@/core/repositories";
+import { RemoveTaskUseCase } from "@/core/usecases/task-usecases";
 import { Request, Response } from "express";
+
+const taskRepository = new MongooseTaskRepository()
+const removeTaskUseCase = new RemoveTaskUseCase(taskRepository)
 
 export const remove = async (req: Request, res: Response) => {
   try {
     const { id } = req.body
 
     
-    const result = await Task.deleteOne({ _id: id })
+    await removeTaskUseCase.execute({ id })
 
-    res.status(200).send({deleted: result.deletedCount > 0 })
+    res.status(200).send({deleted: true })
     
   } catch (error) {
     res.status(400).send({ error: error.message })
