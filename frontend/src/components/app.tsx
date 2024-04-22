ba4bbcda7ae5d52cb6922f67babf944df3b53d3c
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-04-21 22:45:14 -0300

    chore: initial config on frontend

---
+++ frontend/src/components/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Olá mundo</h1>
+    </div>
+  )
+}
