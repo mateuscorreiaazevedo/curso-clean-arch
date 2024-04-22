2514535b73e7ae756cfd5796fbc8592fe1e8e812
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-04-21 22:29:50 -0300

    chore: remove front-end

--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import './assets/styles/globals.css'
-import React from 'react'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
-)
